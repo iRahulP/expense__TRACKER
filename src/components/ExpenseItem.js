7b897010ea22532f672fecab8c74e4ea7c5c8ba5
iRahulP <rahulpatil.m30@gmail.com>
2021-06-14 20:11:55 +0530

    added styles

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import './ExpenseItem.css';
 
 function ExpenseItem() {
     return (
-        <div>
-            <h2>Expense Item</h2>
+        <div className="expense-item">
+            <div>Date</div>
+            <div className="expense-item__description">
+                <h2>Title</h2>
+                <div className="expense-item__price">Amount</div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default ExpenseItem;
