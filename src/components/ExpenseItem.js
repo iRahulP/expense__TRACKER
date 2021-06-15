b002ddf82708786af81a1e0e1cc8528de591de24
iRahulP <rahulpatil.m30@gmail.com>
2021-06-15 10:52:18 +0530

    added JS form

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,12 +2,18 @@ import React from 'react';
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate = new Date().toString().substring(0, 15);
+    const expenseItem = 'Car Insuarance';
+    const expenseAmount = 295.95;
+
     return (
         <div className="expense-item">
-            <div>Date</div>
+            <div>{expenseDate}</div>
             <div className="expense-item__description">
-                <h2>Title</h2>
-                <div className="expense-item__price">Amount</div>
+                <h2>{expenseItem}</h2>
+                <div className="expense-item__price">
+                    {expenseAmount}
+                </div>
             </div>
         </div>
     );
