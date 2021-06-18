1584f4c4343eb0a210a3f0c472fe1f276829c511
rahul <rahulpatil.m30@gmail.com>
2021-06-18 15:31:44 +0530

    nesting props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
     return (
         <div className="expense-item">
-            <div>{props.edate.toString().slice(0,15)}</div>
+            <div>
+                <ExpenseDate 
+                    edate={props.edate}
+                    
+                />
+            </div>
             <div className="expense-item__description">
                 <h2>{props.etitle}</h2>
                 <div className="expense-item__price">
