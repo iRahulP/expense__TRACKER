648d5adc43980974b9cd2456a704096c24210dc4
rahul <rahulpatil.m30@gmail.com>
2021-06-18 15:03:58 +0530

    using props

--- src/App.js
+++ src/App.js
@@ -4,48 +4,41 @@ function App() {
   const expenses = [
     {
       id: "e1",
-      title: "Toilet Paper",
+      title: "Vehicle Check",
       amount: 94.12,
-      date: new Date(2020, 7, 14),
+      date: new Date(2021, 7, 14),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
     {
       id: "e3",
       title: "Car Insurance",
       amount: 294.67,
       date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
+    }
   ];
 
   return (
     <div>
       <h2>Let's get started!</h2>
       <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
+        etitle={expenses[0].title}
+        eamount={expenses[0].amount}
+        edate={expenses[0].date}
       />
       <ExpenseItem 
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
+        etitle={expenses[1].title}
+        eamount={expenses[1].amount}
+        edate={expenses[1].date}
       />
       <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
+        etitle={expenses[2].title}
+        eamount={expenses[2].amount}
+        edate={expenses[2].date}
      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    const expenseDate = new Date().toString().substring(0, 15);
-    const expenseItem = 'Car Insuarance';
-    const expenseAmount = 295.95;
-
+const ExpenseItem = (props) => {
     return (
         <div className="expense-item">
-            <div>{expenseDate}</div>
+            <div>{props.edate.toString().slice(0,15)}</div>
             <div className="expense-item__description">
-                <h2>{expenseItem}</h2>
+                <h2>{props.etitle}</h2>
                 <div className="expense-item__price">
-                    {expenseAmount}
+                    {props.eamount}
                 </div>
             </div>
         </div>
