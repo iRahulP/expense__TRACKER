8b964e6a578defb1c7735b54bc4bd679f8b75e5e
rahul <rahulpatil.m30@gmail.com>
2021-06-22 16:20:01 +0530

    additional 2 way binding added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,27 +1,39 @@
 import ExpenseItem from "../Expenses/ExpenseItem";
-import React from "react";
+import React, { useState } from "react";
+import ExpenseFilter from "./ExpenseFilter";
 import "./Expenses.css";
 import Card from "../UI/Card";
 
 const Expenses = (props) => {
+
+  const [filteredYear, setFilteredYear] = useState('2021');
+
+  const filterChangedHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  }
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+       <ExpenseFilter onChangeFilter={filterChangedHandler} selected={filteredYear} />
+        <ExpenseItem
+          title={props.expenses[0].title}
+          amount={props.expenses[0].amount}
+          date={props.expenses[0].date}
+        />
+        <ExpenseItem
+          title={props.expenses[1].title}
+          amount={props.expenses[1].amount}
+          date={props.expenses[1].date}
+        />
+        <ExpenseItem
+          title={props.expenses[2].title}
+          amount={props.expenses[2].amount}
+          date={props.expenses[2].date}
+        />
+      </Card>
+    </div>
+    
   );
 }
 
