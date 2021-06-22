d624169a70d39487175941358bdb4758949f2a21
rahul <rahulpatil.m30@gmail.com>
2021-06-22 09:33:04 +0530

    combined useStates

--- src/NewExpense/ExpenseForm.js
+++ src/NewExpense/ExpenseForm.js
@@ -3,20 +3,38 @@ import './ExpenseForm.css';
 
 const ExpenseForm = () => {
     
-    const [enteredTitle, setEnteredTitle] = useState('');
-    const [enteredAmount, setEnteredAmount] = useState('');
-    const [enteredDate, setEnteredDate] = useState('');
+    // const [enteredTitle, setEnteredTitle] = useState('');
+    // const [enteredAmount, setEnteredAmount] = useState('');
+    // const [enteredDate, setEnteredDate] = useState('');
+
+    const [userInput, setUserInput] = useState({
+        enteredTitle: '',
+        setEnteredAmount: '',
+        setEnteredDate: ''
+    });
 
     const titleChangeHandler = (e) => {
-        setEnteredTitle(e.target.value);
+        // setEnteredTitle(e.target.value);
+        setUserInput({
+            ...userInput,
+            enteredTitle: e.target.value,
+        })
     }
 
     const amountChangeHandler = (e) => {
-        setEnteredAmount(e.target.value);
+        // setEnteredAmount(e.target.value);
+        setUserInput({
+            ...userInput,
+            setEnteredAmount: e.target.value,
+        })
     }
 
     const dateChangeHandler = (e) => {
-        setEnteredDate(e.target.value);
+        // setEnteredDate(e.target.value);
+        setUserInput({
+            ...userInput,
+            setEnteredDate: e.target.value,
+        })
     }
     
     return (
